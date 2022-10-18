8881425d5af9bae5c561bd7f9e687a2511281fbd
Madhusudan <96856144+madhusudankulkarni@users.noreply.github.com>
2022-10-18 20:43:28 +0530

    added links

--- app.js
+++ app.js
@@ -1,33 +1,33 @@
-const dateOfBirth = document.querySelector("#txtDob");
-const luckyNumber = document.querySelector("#txtLuckyNo");
+const dateOfBirth = document.querySelector('#txtDob');
+const luckyNumber = document.querySelector('#txtLuckyNo');
 
-const modal = document.querySelector("#modal");
-const modalBody = document.querySelector("#modalBody");
-const close = document.querySelector(".close");
+const modal = document.querySelector('#modal');
+const modalBody = document.querySelector('#modalBody');
+const close = document.querySelector('.close');
 
-const checkBtn = document.querySelector("#checkBtn");
+const checkBtn = document.querySelector('#checkBtn');
 
 const checkBirthdateIsLucky = () => {
   const dob = dateOfBirth.value;
   const number = luckyNumber.value;
   const sum = calculateSum(dob);
 
-  if (dob === "" || number === "") {
+  if (dob === '' || number === '') {
     modalBody.innerHTML = `<div><h2>Please fill out the Date of Birth and Your Lucky Number</h2></div>`;
-    modal.style.display = "block";
+    modal.style.display = 'block';
   } else {
     if (sum % number === 0) {
       modalBody.innerHTML = `<div><h2>Your birthday is lucky!</h2></div><div><img src="/assets/happy.gif" width="100%"/></div>`;
-      modal.style.display = "block";
+      modal.style.display = 'block';
     } else {
       modalBody.innerHTML = `<div><h2>Ohoo! Your birthday is not lucky!!</h2></div><div><img src="/assets/sad.gif" width="80%"/></div>`;
-      modal.style.display = "block";
+      modal.style.display = 'block';
     }
   }
 };
 
 const calculateSum = (dob) => {
-  dob = dob.replaceAll("-", "");
+  dob = dob.replaceAll('-', '');
   let sum = 0;
   for (let i = 0; i < dob.length; i++) {
     sum = sum + Number(dob.charAt(i));
@@ -35,16 +35,16 @@ const calculateSum = (dob) => {
   return sum;
 };
 
-checkBtn.addEventListener("click", checkBirthdateIsLucky);
+checkBtn.addEventListener('click', checkBirthdateIsLucky);
 
 close.onclick = () => {
-  modal.style.display = "none";
-  modalBody.innerHTML = "";
+  modal.style.display = 'none';
+  modalBody.innerHTML = '';
 };
 
 window.onclick = function (event) {
   if (event.target === modal) {
-    modal.style.display = "none";
-    modalBody.innerHTML = "";
+    modal.style.display = 'none';
+    modalBody.innerHTML = '';
   }
 };
